113ad4b91d68dd3179c860647b5fc01693318841
nyaruku <38096703+nyaruku@users.noreply.github.com>
2024-03-27 19:03:05 +0300

    Update and rename config_default.h to config.h

---
+++ config.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+static const std::string bot_token = "";
+static const std::string bot_prefix = "-";
+static const std::string bot_owner_id = "";
+static const std::string api_v1_key = "";
+static const std::string api_v2_client_id = "";
+static const std::string api_v2_client_secret = "";
+std::string api_v2_auth_token = "";
+
+// channels
+static const std::string logging_channel_id = "";
+static const std::string new_ranked_channel_id = "";
+static const std::string new_loved_channel_id = "";
+
